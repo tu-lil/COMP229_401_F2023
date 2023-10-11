e61de275587d5039c59d4a4a3241e9fe595e067e
tu-lil <9641540+tu-lil@users.noreply.github.com>
2023-10-10 20:34:37 -0400

    examples of react forms for week 6 and test

---
+++ week_6/2_react_forms_ecentennial/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import Controlled from "./components/Controlled/Controlled";
+
+  const App = () => {
+    return (
+      <div>
+        {<Controlled />}
+      </div>
+    );
+  };
+export default App
